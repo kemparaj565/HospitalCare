9b896cc1a168ac48274e3ff59ac8258113311d89
kemparaj565 <kemparaj565@gmail.com>
2018-03-21 09:17:13 +0530

    Bean Creation with project Dependencies

---
+++ src/main/java/com/hospitalcare/www/hospitalcare/beans/Doctor.java
@@ -0,0 +1,15 @@
+package com.hospitalcare.www.hospitalcare.beans;
+
+public class Doctor {
+	
+	String doctorName;
+	String doctorID;
+	String doctorAddress;
+	String doctorEmailId;
+	Long doctorTelephoneNumber;
+	String doctorQualification;
+	String doctorExpertise;
+	
+	
+
+}
---
+++ src/main/java/com/hospitalcare/www/hospitalcare/beans/Patient.java
@@ -0,0 +1,13 @@
+package com.hospitalcare.www.hospitalcare.beans;
+
+public class Patient {
+	
+	String patientName;
+	String patientID;
+	String patientAddress;
+	String patientEmailId;
+	Long patientTelephoneNumber;
+	String patientGuardian;
+	
+
+}

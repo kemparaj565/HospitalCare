9b57d94b4768d6554fc609ea60ab7422e2fc795c
kemparaj565 <kemparaj565@gmail.com>
2018-03-21 08:45:25 +0530

    Initial Commit

---
+++ src/main/java/com/hospitalcare/www/hospitalcare/App.java
@@ -0,0 +1,13 @@
+package com.hospitalcare.www.hospitalcare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
